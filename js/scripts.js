37c3b7245200fe37f2008f8657384dc7f454398b
Stefano <tepo@hotmail.it>
2021-04-07 15:56:30 +0200

    Creo array di email tramite API

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+var app = new Vue (
+  {
+    el: "#root",
+    data: {
+      mailList: []
+    },
+    mounted: function () {
+      for (let i = 0; i < 10; i++) {
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then( (risposta) => {
+          this.mailList.push(risposta.data.response);
+        });
+      }
+      console.log(this.mailList);
+    }
+  }
+);
